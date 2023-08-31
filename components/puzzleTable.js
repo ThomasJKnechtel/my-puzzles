ea94afa2ca74c01ee9a7b88e7541728b249c4457
Thomas <thomasknechtel@outlook.com>
2023-08-31 03:26:32 -0400

    fixed sorting

--- components/puzzleTable.js
+++ components/puzzleTable.js
@@ -9,7 +9,7 @@ const Chessboard = dynamic(() => import('chessboardjsx'), {
     ssr: false  // <- this do the magic ;)
     });
 
-function Puzzle({id, white, black, date, fen, continuation, puzzles, setPuzzles, dateUploaded, session, saved, turn }){
+function Puzzle({id, white, black, date, fen, continuation, puzzles, setPuzzles, dateUploaded, session, saved, turn, attempts, success_rate }){
     const [isSaved, setSaved] = useState(saved)
     const [puzzleId, setPuzzleId] = useState(id)
     function removePuzzle(){
@@ -42,7 +42,7 @@ function Puzzle({id, white, black, date, fen, continuation, puzzles, setPuzzles,
         })
 
     }
-    return(<tr className="bg-slate-50 p-4 odd:bg-slate-300 "><td className="text-left align-middle"><button className=" border-slate-400 hover:border-2"><Chessboard position={fen} id={fen} width={140}></Chessboard></button></td><td>{white}</td><td>{black}</td><td>{date}</td>{dateUploaded?<td>{dateUploaded}</td>:<td>N/A</td>}<td>{!session?<label>Must be logged in</label>:isSaved?<label>Saved</label>:<button onClick={()=>{savePuzzle()}} className=" text-2xl button-3 bg-blue-500">🖫</button>}</td><td><button onClick={()=>{removePuzzle(puzzles, setPuzzles)}} className="button-3 text-xl font-medium bg-red-700 hover:bg-red-800 "><Image src="https://img.icons8.com/ios/50/delete-trash.png" alt="trash" width={20} height={20} className=""/></button></td><td>{isSaved?<Link className="button-3 green" href={{pathname:"/play/"+puzzleId}}>View</Link>:<label>Must be saved</label>}</td></tr>)
+    return(<tr className="bg-slate-50 p-4 odd:bg-slate-300 "><td className="text-left align-middle"><button className=" border-slate-400 hover:border-2"><Chessboard position={fen} id={fen} width={140}></Chessboard></button></td><td>{white}</td><td>{black}</td><td>{date}</td>{dateUploaded?<td>{dateUploaded}</td>:<td>N/A</td>}<td>{attempts&&<label>{attempts}</label>}</td><td>{success_rate&&<label>{success_rate}</label>}</td><td>{!session?<label>Must be logged in</label>:isSaved?<label>Saved</label>:<button onClick={()=>{savePuzzle()}} className=" text-2xl button-3 bg-blue-500">🖫</button>}</td><td><button onClick={()=>{removePuzzle(puzzles, setPuzzles)}} className="button-3 text-xl font-medium bg-red-700 hover:bg-red-800 "><Image src="https://img.icons8.com/ios/50/delete-trash.png" alt="trash" width={20} height={20} className=""/></button></td><td>{isSaved?<Link className="button-3 green" href={{pathname:"/play/"+puzzleId}}>View</Link>:<label>Must be saved</label>}</td></tr>)
   
 }
 
@@ -56,7 +56,7 @@ export default function PuzzleTable({puzzles, setPuzzles, session, saved}){
         
         <table className=" w-full table-fixed">
             
-            <thead className=" bg-slate-300 mb-2  shadow-lg h-10 sticky top-0 z-10"><tr className="h-14 sticky top-0 z-10" ><th className=" sticky top-0 z-10"></th><th  className=" sticky top-0 z-10">White</th><th className="sticky top-0 z-10">Black</th><th className="sticky top-0 z-10">Date</th><th className=" sticky top-0 z-10">Date Uploaded</th>{session&&<th className="w-13 sticky top-0 z-10"><button onClick={()=>setPuzzles([])} className=" text-2xl button-3 bg-blue-500">🖫</button></th>}<th className="w-13 sticky top-0 z-10"><button onClick={()=>setPuzzles([])} className="button-3 font-medium text-sm bg-red-700 hover:bg-red-800"><Image src="https://img.icons8.com/ios/50/delete-trash.png" alt="trash" width={20} height={20} className=""/></button></th><th className="" sticky top-0 z-10>View</th></tr></thead>
+            <thead className=" bg-slate-300 mb-2  shadow-lg h-10 sticky top-0 z-10"><tr className="h-14 sticky top-0 z-10" ><th className=" sticky top-0 z-10"></th><th  className=" sticky top-0 z-10">White</th><th className="sticky top-0 z-10">Black</th><th className="sticky top-0 z-10">Date</th><th className=" sticky top-0 z-10">Date Uploaded</th><th className=" sticky top-0 z-10">Attempts</th><th className=" sticky top-0 z-10">Success Rate</th>{session&&<th className="w-13 sticky top-0 z-10"><button onClick={()=>setPuzzles([])} className=" text-2xl button-3 bg-blue-500">🖫</button></th>}<th className="w-13 sticky top-0 z-10"><button onClick={()=>setPuzzles([])} className="button-3 font-medium text-sm bg-red-700 hover:bg-red-800"><Image src="https://img.icons8.com/ios/50/delete-trash.png" alt="trash" width={20} height={20} className=""/></button></th><th className="" sticky top-0 z-10>View</th></tr></thead>
          <tbody>
              <tr className="h-10"></tr>
          </tbody>   
@@ -65,7 +65,7 @@ export default function PuzzleTable({puzzles, setPuzzles, session, saved}){
             {   puzzles &&(
                  puzzles.map((puzzle) =>{
                         if(puzzle){
-                             return <Puzzle id={puzzle.puzzle_id} white={puzzle.white} black = {puzzle.black} date = {formatDate(puzzle.date)} fen = {puzzle.fen} continuation={puzzle.continuation} puzzles={puzzles} setPuzzles={setPuzzles} dateUploaded={puzzle.dateUploaded?formatDate(puzzle.date_uploaded):"N/A" } saved={saved}session={session} turn={puzzle.turn}></Puzzle>
+                             return <Puzzle id={puzzle.puzzle_id} white={puzzle.white} black = {puzzle.black} date = {formatDate(puzzle.date)} fen = {puzzle.fen} continuation={puzzle.continuation} puzzles={puzzles} setPuzzles={setPuzzles} dateUploaded={puzzle.dateUploaded?formatDate(puzzle.date_uploaded):"N/A" } saved={saved}session={session} turn={puzzle.turn} attempts={puzzle.attempts} success_rate={puzzle.success_rate}></Puzzle>
                         }
                        
                     })
--- pages/api/db/getPuzzles.js
+++ pages/api/db/getPuzzles.js
@@ -1,14 +1,16 @@
 import db from "@/utils/dbConnect"
 export default async function getPuzzles(searchParams){
-    let {player, opponent, startDate, endDate, startDateUploaded, endDateUploaded, numberOfPuzzles} = searchParams
+    let {player, opponent, startDate, endDate, startDateUploaded, endDateUploaded, numberOfPuzzles, sortCriteria} = searchParams
     player = player?("'"+player+"'"):null
     opponent= opponent?("'"+opponent+"'"):null
     startDate=startDate?(startDate):null
     endDate=endDate?(endDate):null
     startDateUploaded=startDateUploaded?(startDateUploaded):null
     endDateUploaded=endDateUploaded?(endDateUploaded):null
-    numberOfPuzzles=numberOfPuzzles?(numberOfPuzzles):20
-    const query = `EXEC get_puzzles @player=${player}, @opponent=${opponent}, @fromDatePlayed=${startDate}, @toDatePlayed=${endDate}, @fromDateUploaded=${startDateUploaded}, @toDateUploaded=${endDateUploaded}, @attempts=NULL, @success_rate=NULL, @sortCriteria='datePlayed', @numberOfPuzzles=${numberOfPuzzles}, @username=NULL`
+    numberOfPuzzles=(numberOfPuzzles>0)?(numberOfPuzzles):20
+    sortCriteria = sortCriteria?(sortCriteria):null
+    const query = `EXEC get_puzzles @player=${player}, @opponent=${opponent}, @fromDatePlayed=${startDate}, @toDatePlayed=${endDate}, @fromDateUploaded=${startDateUploaded}, @toDateUploaded=${endDateUploaded},  @sortCriteria='${sortCriteria}', @numberOfPuzzles=${numberOfPuzzles}, @username=NULL`
+    console.log(query)
     const result = await db.query(query)
     return result.recordsets[0]
 }
